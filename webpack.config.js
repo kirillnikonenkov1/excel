9ce05482ff1c5e2dcf3f19921280e438ac9cd3bd
kirillnikonenkov1 <47380817+kirillnikonenkov1@users.noreply.github.com>
2021-02-13 14:39:34 +0300

    Feature/webpack (#1)
    * create webpack config
    
    * feat(webpack): добавлена базовая конфигурация webpack
    
    * feat(git): добвит pacake-lock.json в gitignore
    
    Co-authored-by: nikonenkov <nikonenkov@ucoz-team.net>

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const fileName = (ext) => isProd ? `bundle.[hash].${ext}` : `bundl.${ext}`
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ]
+
+  if (!isProd) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: fileName('js'),
+  },
+  devtool: isProd ? false : 'source-map',
+  devServer: {
+    port: 3000,
+    hot: !isProd,
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+}
